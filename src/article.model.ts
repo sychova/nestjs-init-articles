950622fc68ac923edac14e45b812583ee0314175
sychova <anastasiya.sychova@gmail.com>
2024-10-18 17:24:58 +0300

    add article model

---
+++ src/article.model.ts
@@ -0,0 +1,11 @@
+export class Article {
+  id: number;
+  title: string;
+  content: string;
+
+  constructor(title: string, content: string, id?: number) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+}
