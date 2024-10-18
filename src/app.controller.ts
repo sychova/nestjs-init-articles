2af8aaf89d2ad7a64015b0f87663e9aa3b6ff91b
sychova <anastasiya.sychova@gmail.com>
2024-10-18 18:13:41 +0300

    finish articles

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Render,
+  Param,
+  ParseIntPipe,
+  Body,
+  Post,
+  Redirect,
+} from '@nestjs/common';
 import { articles } from './articles';
+import { Article } from './article.model';
 
 @Controller()
 export class AppController {
   @Get()
+  @Render('index')
   index() {
     return { articles };
+  }
+
+  @Get('create')
+  @Render('create-article')
+  getForm(): void {
+    return;
+  }
+
+  @Post('articles')
+  @Redirect('/', 301)
+  create(@Body() body: any): void {
+    const id = articles.length + 1;
+    const article = new Article(body.title, body.content, id);
+    articles.push(article);
+  }
+
+  @Get(':id')
+  @Render('article')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return articles.find((article) => article.id === id);
   }
 }
